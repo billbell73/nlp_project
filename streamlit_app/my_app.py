05d75f7987a3c150f780a6df5b5c83756127583b
William Bell <willbell@gmail.com>
2022-02-21 15:32:36 +0000

    update to app - use corex

--- streamlit_app/my_app.py
+++ streamlit_app/my_app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 from scipy.spatial import distance
-import pandas as pd
 import pickle5 as pickle
 
-with open('doc_topic_5.pickle', 'rb') as handle:
-    doc_topic_5 = pickle.load(handle)
+with open('doc_topic_corex.pickle', 'rb') as handle:
+    doc_topic = pickle.load(handle)
 
 with open('big_job_df.pickle', 'rb') as handle2:
     df = pickle.load(handle2)
@@ -13,11 +12,9 @@ def clear_pick():
     if 'jobs' in st.session_state:
         st.session_state.pop('jobs')
 
-def best_jobs(input_0, input_1, level, df):
-    value_0 = input_0/1000
-    value_1 = input_1/1000
-    target = [value_0, value_1, 0, 0, 0]
-    distances = distance.cdist([target], doc_topic_5, "cosine")[0]
+def best_jobs(topic_inputs, level):
+    target = [ element / 10 for element in topic_inputs]
+    distances = distance.cdist([target], doc_topic, "cosine")[0]
     sorted_indexes = distances.argsort()
 
     jobs = []
@@ -33,17 +30,23 @@ def best_jobs(input_0, input_1, level, df):
 
 
 st.title('Linkedin job listing finder')
+st.sidebar.write('**Choose your liking out of 10 for the following:**')
 
-topic_0_input = st.sidebar.slider('Pick a number for topic 0', min_value=0, max_value=135, step=1, on_change=clear_pick)
+input_0 = st.sidebar.slider('Communication with the business', 0, 10, on_change=clear_pick)
+input_1 = st.sidebar.slider('Engineering', 0, 10, on_change=clear_pick)
+input_2 = st.sidebar.slider('Modelling / developing models', 0, 10, on_change=clear_pick)
+input_3 = st.sidebar.slider('Work/life balance', 0, 10, on_change=clear_pick)
+input_4 = st.sidebar.slider('Startup-like environment', 0, 10, on_change=clear_pick)
 
-topic_1_input = st.sidebar.slider('Pick a number for topic 1', 0, 124, on_change=clear_pick)
-
-# this stype hack was taken from https://discuss.streamlit.io/t/horizontal-radio-buttons/2114/3
+# this style hack was taken from https://discuss.streamlit.io/t/horizontal-radio-buttons/2114/3
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
-level = st.sidebar.radio("Experience Level:", ('Junior', 'Mid', 'Senior'), index=1, on_change=clear_pick)
+st.sidebar.write("")
+level = st.sidebar.radio('Experience level:', ('Junior', 'Mid', 'Senior'), index=1, on_change=clear_pick)
 
-st.sidebar.button("Search", on_click=best_jobs, args=(topic_0_input, topic_1_input, level, df))
+topic_inputs = [input_0, input_1, input_2, input_3, input_4]
+
+st.sidebar.button("Search", on_click=best_jobs, args=(topic_inputs, level))
 
 if 'jobs' in st.session_state:
     for i, job in enumerate(st.session_state.jobs):
