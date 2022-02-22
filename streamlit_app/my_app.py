9c3437f72e1201575c51d8e0bce18b37eec210f0
William Bell <willbell@gmail.com>
2022-02-22 14:23:05 +0000

    final iteration of app with CorEx

--- streamlit_app/my_app.py
+++ streamlit_app/my_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 from scipy.spatial import distance
 import pickle5 as pickle
 
-with open('doc_topic_corex.pickle', 'rb') as handle:
+with open('../doc_topic_corex.pickle', 'rb') as handle:
     doc_topic = pickle.load(handle)
 
-with open('big_job_df.pickle', 'rb') as handle2:
+with open('../big_job_df.pickle', 'rb') as handle2:
     df = pickle.load(handle2)
 
 def clear_pick():
@@ -28,15 +28,23 @@ def best_jobs(topic_inputs, level):
 
     st.session_state.jobs = jobs
 
+col1, col2 = st.columns([25, 30])
+with col1:
+    st.image('LI-Logo.png', width=300)
+with col2:
+    if 'jobs' in st.session_state:
+        st.write("")
+        st.subheader('**Data Science job listing finder**')
 
-st.title('Linkedin job listing finder')
+if 'jobs' not in st.session_state:
+    st.title('Data Science job listing finder')
 st.sidebar.write('**Choose the extent (out of 10) you want your job to involve:**')
 
-input_0 = st.sidebar.slider('Communication with stakeholders', 0, 10, on_change=clear_pick)
-input_1 = st.sidebar.slider('Engineering', 0, 10, on_change=clear_pick)
-input_2 = st.sidebar.slider('Modelling / developing models', 0, 10, on_change=clear_pick)
-input_3 = st.sidebar.slider('Good work environment and work-life balance', 0, 10, on_change=clear_pick)
-input_4 = st.sidebar.slider('Startup-like environment', 0, 10, on_change=clear_pick)
+input_0 = st.sidebar.slider('Communication with stakeholders', 1, 10, 5, on_change=clear_pick)
+input_1 = st.sidebar.slider('Engineering', 1, 10, 5, on_change=clear_pick)
+input_2 = st.sidebar.slider('Modelling / developing models', 1, 10, 5, on_change=clear_pick)
+input_3 = st.sidebar.slider('Good work environment and work-life balance', 1, 10, 5, on_change=clear_pick)
+input_4 = st.sidebar.slider('Startup-like environment', 1, 10, 5, on_change=clear_pick)
 
 # this style hack was taken from https://discuss.streamlit.io/t/horizontal-radio-buttons/2114/3
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
